5e13d80654f313c4b6e0c3abb70b29a06457deb7
xiaopies <xiaoshenma2016@gmail.com>
2021-01-05 04:07:07 -0500

    tested two methods

---
+++ Tester.java
@@ -0,0 +1,13 @@
+public class Tester{
+  public static void main(String[] args){
+    System.out.println(PigLatin.pigLatinSimple("mock"));
+    System.out.println(PigLatin.pigLatinSimple("pie"));
+    System.out.println(PigLatin.pigLatinSimple("david"));
+    System.out.println(PigLatin.pigLatinSimple("aaron"));
+    System.out.println(PigLatin.pigLatin("the"));
+    System.out.println(PigLatin.pigLatin("check"));
+    System.out.println(PigLatin.pigLatin("skee"));
+    System.out.println(PigLatin.pigLatin("emu"));
+    System.out.println(PigLatin.pigLatin("grade"));
+  }
+}
