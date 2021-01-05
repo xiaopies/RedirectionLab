53f134196f66ee3d0f9c153677f7ac47b89a8c0b
xiaopies <xiaoshenma2016@gmail.com>
2021-01-05 03:53:19 -0500

    added pigLatin

--- PigLatin.java
+++ PigLatin.java
@@ -11,6 +11,25 @@ public class PigLatin{
     }
 
   }
+  public static String pigLatin(String s){
+    String a = s.toLowerCase();
+    String b = s.substring(0, 2);
+    String c = s.substring(0,1);
+    String[] hi = new String[] {"bl", "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr", "gh", "gl", "gr", "ng", "ph", "pl", "pr", "qu", "sc", "sh", "sk", "sl", "sm", "sn", "sp", "st", "sw", "th", "tr", "tw", "wh", "wr"};
+    for (int i=0; i<hi.length; i++){
+      if (b.equals(hi[i])){
+        return(a.substring(2, a.length()) + hi[i] + "ay");
+      }
+    }
+    if (c.equals("a")|| c.equals("e")|| c.equals("i")|| c.equals("o")|| c.equals("u")){
+      return (a + "hay");
+    }
+    else{
+      return (a.substring(1,a.length()) + b + "ay");
+    }
+
+
+  }
   // Scanner me = new Scanner(System.in);
   // while (me.hasNextline()) {
   //   Scanner line = new Scanner(me.nextLine());
