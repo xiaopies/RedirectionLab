f397c4fd725786a5b595f121b19fc028ba6cea73
xiaopies <xiaoshenma2016@gmail.com>
2021-01-05 04:48:33 -0500

    added main

--- PigLatin.java
+++ PigLatin.java
@@ -13,19 +13,20 @@ public class PigLatin{
   }
   public static String pigLatin(String s){
     String a = s.toLowerCase();
-    String b = s.substring(0, 2);
     String c = s.substring(0,1);
     String[] hi = new String[] {"bl", "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr", "gh", "gl", "gr", "ng", "ph", "pl", "pr", "qu", "sc", "sh", "sk", "sl", "sm", "sn", "sp", "st", "sw", "th", "tr", "tw", "wh", "wr"};
+    if (s.length()>=2){
     for (int i=0; i<hi.length; i++){
-      if (b.equals(hi[i])){
+      if (a.substring(0,2).equals(hi[i])){
         return(a.substring(2, a.length()) + hi[i] + "ay");
       }
     }
+  }
     if (c.equals("a")|| c.equals("e")|| c.equals("i")|| c.equals("o")|| c.equals("u")){
       return (a + "hay");
     }
     else{
-      return (a.substring(1,a.length()) + b + "ay");
+      return (a.substring(1,a.length()) + c + "ay");
     }
   }
   public static String pigLatinBest(String s){
@@ -44,6 +45,19 @@ public class PigLatin{
         return(pigLatin(s));
       }
     }
+  }
+  public static void main(String[] args){
+    Scanner me = new Scanner(System.in);
+    while (me.hasNextLine()) {
+      Scanner line = new Scanner(me.nextLine());
+      while (line.hasNext()) {
+        String a = line.next();
+        System.out.print(pigLatinBest(a));
+        System.out.print(" ");
+      }
+      System.out.print("\n");
+    }
+
   }
   // Scanner me = new Scanner(System.in);
   // while (me.hasNextline()) {
